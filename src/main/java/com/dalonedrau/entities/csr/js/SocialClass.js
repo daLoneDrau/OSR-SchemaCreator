17ecd19fca4640f15f725ca4dc77e22aaa660305
daLoneDrau <donaldbrau@yahoo.com>
2017-03-12 22:39:18 -0400

    Sun, Mar 12, 2017 10:38:52 PM

--- src/main/java/com/dalonedrau/entities/csr/js/SocialClass.js
+++ src/main/java/com/dalonedrau/entities/csr/js/SocialClass.js
@@ -5,9 +5,57 @@
 	"roll_max": 10,
 	"pointsAdjustment": 4
 }, {
-	"name": "SERVILE",
-	"title": "Villein",
-	"roll_min": 1,
-	"roll_max": 10,
-	"pointsAdjustment": 4
+	"name": "FREEMAN",
+	"title": "Freeman",
+	"roll_min": 11,
+	"roll_max": 50,
+	"pointsAdjustment": 0
+}, {
+	"name": "TOWNSMAN",
+	"title": "Townsman",
+	"roll_min": 51,
+	"roll_max": 70,
+	"pointsAdjustment": 0
+}, {
+	"name": "GUILDSMAN",
+	"title": "Guildsman",
+	"roll_min": 71,
+	"roll_max": 75,
+	"pointsAdjustment": -4
+}, {
+	"name": "LANDLESS KNIGHT",
+	"title": "Landless Gentry",
+	"roll_min": 76,
+	"roll_max": 80,
+	"pointsAdjustment": -6
+}, {
+	"name": "LANDED KNIGHT",
+	"title": "Landed Gentry",
+	"roll_min": 81,
+	"roll_max": 90,
+	"pointsAdjustment": -9
+}, {
+	"name": "BANNERETTE",
+	"title": "Bannerette",
+	"roll_min": 91,
+	"roll_max": 94,
+	"pointsAdjustment": -12
+}, {
+	"name": "BARON",
+	"title": "Baron",
+	"roll_min": 95,
+	"roll_max": 97,
+	"pointsAdjustment": -15
+}, {
+	"name": "TITLED NOBLE",
+	"title": "Nobility",
+	"roll_min": 98,
+	"roll_max": 99,
+	"pointsAdjustment": -20
+}, {
+	"name": "ROYALTY",
+	"title": "Royalty",
+	"roll_min": 100,
+	"roll_max": 100,
+	"pointsAdjustment": -25
 }]
