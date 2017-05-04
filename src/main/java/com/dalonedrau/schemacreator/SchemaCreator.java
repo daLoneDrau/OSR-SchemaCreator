b5cc7546f3d2ceb721355b3b35604c4877e5cf72
daLoneDrau <donaldbrau@yahoo.com>
2017-05-04 16:30:46 -0400

    Thu, May 04, 2017  4:30:09 PM

--- src/main/java/com/dalonedrau/schemacreator/SchemaCreator.java
+++ src/main/java/com/dalonedrau/schemacreator/SchemaCreator.java
@@ -58,10 +58,10 @@ public final class SchemaCreator {
                 // "com.dalonedrau.entities.avalon",
                 // "com.dalonedrau.entities.bp",
                 // "com.dalonedrau.entities.csr",
-                // "com.dalonedrau.entities.ff",
+                "com.dalonedrau.entities.ff",
                 // "com.dalonedrau.entities.ll",
                 // "com.dalonedrau.entities.lablord",
-                "com.dalonedrau.entities.tft",
+                // "com.dalonedrau.entities.tft",
                 // "com.dalonedrau.entities.wfrp"
         };
         final SchemaCreator[] creators = new SchemaCreator[pkgs.length];
